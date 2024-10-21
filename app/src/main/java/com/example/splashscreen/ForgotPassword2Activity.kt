cef90832fac8455a242c52cd4074a432dace7434
Yashin Al Fauzy <yashinfauzy@apps.ipb.ac.id>
2024-10-21 08:18:07 +0700

    update login and sign up

--- app/src/main/java/com/example/splashscreen/ForgotPassword2Activity.kt
+++ app/src/main/java/com/example/splashscreen/ForgotPassword2Activity.kt
@@ -1,21 +1,28 @@
-package com.example.poultrylink_mobile
+package com.example.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ForgotPassword2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.forgot_password2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val ButtonNext = findViewById<Button>(R.id.ButtonConfirmPassword)
 
+        ButtonNext.setOnClickListener{
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/splashscreen/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/splashscreen/ForgotPasswordActivity.kt
@@ -1,21 +1,29 @@
-package com.example.poultrylink_mobile
+package com.example.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.forgot_password)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        val buttonNext = findViewById<Button>(R.id.verificationForgotPassword)
+
+        buttonNext.setOnClickListener {
+            val intent = Intent(this, ForgotPassword2Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
